dd863f6b7a1ae95e7ce2816e179b93792da5dc6a
Pedro Santos <github@mariodelrio.com>
2016-12-05 17:06:40 +0000

    Fixed build issues

--- build.sh
+++ build.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
+if [ -f ".paket/paket.exe" ]
+then
+   echo "paket.exe is available. So skipping paket.bootstrapper"
+else
+  mono .paket/paket.bootstrapper.exe
+fi
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+  exit $exit_code
+fi
+mono .paket/paket.exe restore
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+	exit $exit_code
+fi
+export MONO_MANAGED_WATCHER=false
 mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe build.fsx $@
