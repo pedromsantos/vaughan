1646849b40cb1463c3b8be9641427bf1cb430ac7
Pedro Santos <pedro@codurance.com>
2017-05-15 17:02:25 +0100

    Add env variable for git api

--- paket.ps1
+++ paket.ps1
@@ -1,3 +1,3 @@
-$latestRelease = Invoke-RestMethod https://api.github.com/repos/fsprojects/Paket/releases/latest
+$latestRelease = Invoke-RestMethod https://api.github.com/repos/fsprojects/Paket/releases/latest?access_token=TSD_GITHUB_TOKEN
 $latestReleaseURL = $latestRelease.assets.browser_download_url[0]
 Invoke-WebRequest -Uri $latestReleaseURL -OutFile .paket/paket.bootstrapper.exe
