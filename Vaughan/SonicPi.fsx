9e7730012730da84fe3561dba0efa8201d0abc85
Pedro Santos <pedro@codurance.com>
2017-08-15 15:10:16 +0100

    Update SonicPi.fsx and README with latest DSL

--- Vaughan/SonicPi.fsx
+++ Vaughan/SonicPi.fsx
@@ -14,7 +14,10 @@ open Vaughan.SonicPi
 Statments[
     WithSynth(Fm, [
                 WithFx(Reverb, [
-                            PlayNote(C, OneLine, [Amplitude(5.0<loud>); Panning(-1.0<pan>)]);
+                            PlayNote(C, OneLine, [Amplitude(0.5<loud>); 
+                                                    Panning(0.0<pan>); 
+                                                    Attack(2.0<beat>); 
+                                                    Release(1.0<beat>)])
                             Sleep(1<s>);
                             PlayChord(chord C Major, TwoLine, [Amplitude(1.0<loud>); Panning(1.0<pan>)])
                             ])
