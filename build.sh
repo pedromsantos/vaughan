6583932c0c1487620f9dca0ec3278507cf6c3c47
vorotato <vash.arclan@gmail.com>
2018-02-12 23:20:43 -0500

    Forgot to add paket bootstrapper call for linux

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ then
   packages/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx
 else
   # use mono
+  mono  .paket/paket.bootstrapper.exe
   mono .paket/paket.exe restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
